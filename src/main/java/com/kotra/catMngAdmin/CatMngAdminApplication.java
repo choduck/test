e0ad653ac0ebfe0cecb41168da100d495b1b9fbc
DESKTOP-PI5E4V1\chodu <choduck99@gmail.com>
2021-04-14 16:15:26 +0900

    .

---
+++ src/main/java/com/kotra/catMngAdmin/CatMngAdminApplication.java
@@ -0,0 +1,13 @@
+package com.kotra.catMngAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatMngAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatMngAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kotra/catMngAdmin/CatMngAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotra.catMngAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatMngAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
